93ad2428eb2c3ba19c253f6cb237dda851714962
kou7306 <kouta.1206.y@icloud.com>
2024-06-30 18:25:26 +0900

    ws

--- src/routes/wsRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { chatWebSocket } from "../controllers/wsController";
-
-const router = Router();
-
-router.get("/chat", chatWebSocket);
-
-export default router;
