1362b65f6d5ce79c6f516751e648e7a13a7093ef
na2kera <nat3mitaro@gmail.com>
2024-06-30 16:50:48 +0900

    APIの修正

--- src/controllers/likeController.ts
+++ src/controllers/likeController.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import { createLikeService } from "../services/likeService";
 
 export const createLike = async (req: Request, res: Response) => {
+  console.log("Received data:", req.body);
   try {
-    const { uuid, ids } = req.body;
-    await createLikeService(uuid, ids);
+    const { uuid, IDs } = req.body;
+    await createLikeService(uuid, IDs);
     res.status(200).json({ message: "Likes created successfully" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", details: error });
