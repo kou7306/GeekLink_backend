f9a5a1c2ff53c9ec3850401438b9a1bc63ed78d7
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-10-02 08:53:31 +0900

    feat: routeとcontrollerの実装

--- src/controllers/githubController.ts
+++ src/controllers/githubController.ts
@@ -1,13 +1,17 @@
-import express, { Request, Response } from 'express';
-import { graphql } from '../graphql/graphql';
-import { githubCallBackService } from '../services/githubService';
-import { getRepositoryQuery } from '../graphql/queries/getUserGithub';
+import { Request, Response } from 'express';
+import { 
+  githubCallBackService, 
+  getUserGithubInfo,
+  getRepostiroryService,
+  getContributionService,
+  getActivityLogService,
+  getUseLanguagesService
+} from '../services/githubService';
 
 // OAuthでアクセストークンを取得
 export const githubCallback = async (req: Request, res: Response) => {
   const { uuid, code } = req.body;
   try {
-    console.log("uuid:", uuid);
     const client_id = process.env.GITHUB_CLIENT_ID;
     const client_secret = process.env.GITHUB_CLIENT_SECRET;
     if (client_id == undefined || client_secret == undefined) return;
@@ -18,21 +22,63 @@ export const githubCallback = async (req: Request, res: Response) => {
   }
 };
 
-// ユーザーのGitHubから情報を取得(公開リポジトリのみ)
-
+// ユーザーのGitHubから情報を取得
 // ユーザーのリポジトリ情報を取得(最新10件)
 export const getRepository = async (req: Request, res: Response) => {
+  const { uuid } = req.body;
   try {
-    const userName = req.params.username;
+    const { username, token } = await getUserGithubInfo(uuid);
 
-    const repositories = await graphql(
-      getRepositoryQuery,
-      { userName },
-    );
+    const repositories = await getRepostiroryService(username, token);
 
     res.json(repositories);
   } catch(error) {
     console.error("Error in getRepostitory:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// ユーザーのコントリビューション数を取得
+export const getContribution = async (req: Request, res: Response) => {
+  const { uuid } = req.body;
+  try {
+    const { username, token } = await getUserGithubInfo(uuid);
+
+    const contributions = await getContributionService(username, token);
+
+    res.json(contributions);
+  } catch(error) {
+    console.error("Error in getContributions:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// ユーザーの直近のアクティビティログを取得
+export const getActivityLog = async (req: Request, res: Response) => {
+  const { uuid } = req.body;
+  try {
+    const { username, token } = await getUserGithubInfo(uuid);
+
+    const logs = await getActivityLogService(username, token);    
+
+    res.json(logs);
+  } catch(error) {
+    console.error("Error in getActivityLog:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// ユーザーの使用言語量(割合)を取得
+export const getUseLanguage = async (req: Request, res: Response) => {
+  const { uuid } = req.body;
+  try {
+    const { username, token } = await getUserGithubInfo(uuid);
+
+    const logs = await getUseLanguagesService(username, token);    
+
+    res.json(logs);
+  } catch(error) {
+    console.error("Error in getUseLanguage:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- src/routes/githubRoute.ts
+++ src/routes/githubRoute.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import {
+  githubCallback,
   getRepository,
-  githubCallback
+  getContribution,
+  getActivityLog,
+  getUseLanguage
 } from "../controllers/githubController";
 
 const router = Router();
 
 router.post("/callback", githubCallback);
 // TODO: routeは仮置き
-router.post("/repo/:username", getRepository);
+router.post("/repo", getRepository);
+router.post("/cont", getContribution);
+router.post("/log", getActivityLog);
+router.post("/lang", getUseLanguage)
 
 export default router;
