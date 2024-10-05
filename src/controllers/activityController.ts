6d1d1689818baa38d04a82f9fe53a06e08efe41f
kou7306 <kouta.1206.y@icloud.com>
2024-10-05 12:11:45 +0900

    qiitaのアクティビティーログに必要なAPI

--- src/controllers/activityController.ts
+++ src/controllers/activityController.ts
@@ -3,6 +3,7 @@ import {
   getUserGithubInfo,
   getActivityLogService,
 } from "../services/githubService";
+import { getQiitaUserActivityService } from "../services/qiitaService";
 
 export const getGithubActivity = async (req: Request, res: Response) => {
   const { uuid, time } = req.body;
@@ -17,6 +18,19 @@ export const getGithubActivity = async (req: Request, res: Response) => {
     res.json(result);
   } catch (error) {
     console.error("Error in getRepostitory:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getQiitaActivity = async (req: Request, res: Response) => {
+  const { uuid, period } = req.body;
+  try {
+    const activity = await getQiitaUserActivityService(uuid, period);
+
+    // 取得した活動データをそのままJSON形式で返す
+    res.status(200).json(activity);
+  } catch (error) {
+    console.error("Error in getQiitaActivity:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
